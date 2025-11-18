3c81d6aa3e511186f7495a64381b56eeacb59521
EFTC-NTMK <vbtst001@gmail.com>
2025-11-18 18:06:10 +0700

    first commit

---
+++ plus.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int a,b;
+
+void Input(){
+
+
+}
+
+void Solve(){
+
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    Input();
+    Solve();
+    return 0;
+}
